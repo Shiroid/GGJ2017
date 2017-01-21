5d1d096bc4f9dfb6f6927d416d220379d102e814
DESKTOP-5K867SI\jelmer <Jelmerlukkien@live.nl>
2017-01-21 10:23:16 +0100

    Added extra polish to level

--- Assets/Scripts/Game/PhotographerController.cs
+++ Assets/Scripts/Game/PhotographerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PhotographerController : MonoBehaviour {
 
@@ -10,6 +11,9 @@ public class PhotographerController : MonoBehaviour {
     public float photoFreeze = 3f;
     private float remainingTime;
     public string nextScene = "Test Scene";
+    public Text countdownUI;
+    public AudioSource clock;
+    public SpriteRenderer Overlay;
 
     public GameObject flash;
 
@@ -23,22 +27,32 @@ public class PhotographerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         remainingTime -= Time.deltaTime;
+        countdownUI.text = Mathf.Round(remainingTime).ToString();
         if (remainingTime < waitTime)
         {
             //Allow movement
             GlobalVariables.canMove = true;
+            if (!clock.isPlaying)
+            {
+                clock.Play();
+            }
         }
         if (remainingTime < 0)
         {
             //Snap photo
             if(flash) flash.SetActive(true);
             GlobalVariables.canMove = false;
+            countdownUI.text = "";
+            clock.Stop();
+            clock.time = 0.0f;
+            Overlay.color = new Color(1f, 1f, 1f, 0f);
         }
         if (remainingTime < -photoFreeze)
         {
             //Transition to next scene
             if (Config.savePhotos) Application.CaptureScreenshot(SceneManager.GetActiveScene().name + ".png");
             SceneManager.LoadScene(nextScene);
+            Overlay.color = new Color(1f, 1f, 1f, 1f);
         }
 	}
 }
